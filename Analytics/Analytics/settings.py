93ac56044c304ad26831cde21281c9112ad3d92b
SatishSahani <satishsahani951@gmail.com>
2023-08-08 17:19:35 +0530

    modified code

--- Analytics/Analytics/settings.py
+++ Analytics/Analytics/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2tgh2$g!fd05=uq7(u&k4_8qj3k@x5pj77!2lwqzqa#(ga&z1)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
